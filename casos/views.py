8af1cb99d6a060a9d4cc37fdc695d6cc0b6616a7
Mario Miguel Zepeda Bacca <mzepeda@sar.gob.hn>
2017-10-24 16:50:53 -0600

    Added Theme to Account Views

--- casos/views.py
+++ casos/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
+from django.contrib.auth.decorators import login_required
+
 from .models import Caso
 
 # Create your views here.
 
-
+@login_required(login_url='/account/login/')
 def caso_list(request):
     casos = Caso.objects.all()
 
