28e177ea0b3137f1ee114ed9eda436e58751ef09
Mario Miguel Zepeda Bacca <mzepeda@sar.gob.hn>
2017-10-31 15:15:30 -0600

    Added admin functionality

--- casos/admin.py
+++ casos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericStackedInline
 from django import forms
 from django.contrib.auth.models import User
 
@@ -6,7 +7,8 @@ from django.contrib.auth.models import User
 
 from casos.models import (
     Caso,
-    Aseguradora
+    Aseguradora,
+    Avance
 )
 
 
@@ -20,6 +22,13 @@ class CasoAdminForm(forms.ModelForm):
           'transmision': forms.RadioSelect,
           'personal': forms.RadioSelect,
         }
+
+
+class AvanceInline(admin.StackedInline):
+    model = Avance
+    readonly_fields = ['fecha_creado']
+    extra = 0
+    show_change_link = False
 
 class CasoAdmin(admin.ModelAdmin):
     form = CasoAdminForm
@@ -35,11 +44,18 @@ class CasoAdmin(admin.ModelAdmin):
     list_display = ["id","descripcion", "fecha_creado"]
     search_fields = ['id', "cliente__username"]
     raw_id_fields = ('cliente',)
+    inlines = [
+        AvanceInline,
+    ]
 
 
 class AseguradoraAdmin(admin.ModelAdmin):
     list_display = ["id", "nombre"]
     search_fields = ['nombre']
 
+class AvanceAdmin(admin.ModelAdmin):
+    model = Avance
+
+admin.site.register(Avance, AvanceAdmin)
 admin.site.register(Caso, CasoAdmin)
 admin.site.register(Aseguradora, AseguradoraAdmin)
--- casos/models.py
+++ casos/models.py
@@ -46,4 +46,15 @@ class Caso(models.Model):
     def __str__(self):              # __unicode__ on Python
    		return "Caso Id# " + str(self.id)
 
+class Avance(models.Model):
+    fecha_creado = models.DateTimeField(auto_now_add=True)
+    descripcion = models.CharField(max_length=200)
+    caso = models.ForeignKey(Caso, on_delete=models.CASCADE)
+    imagen_1 = models.ImageField(upload_to='images/', null=True)
+    imagen_2 = models.ImageField(upload_to='images/', null=True)
+    imagen_3 = models.ImageField(upload_to='images/', null=True)
+    imagen_4 = models.ImageField(upload_to='images/', null=True)
+    imagen_5 = models.ImageField(upload_to='images/', null=True)
 
+    def __str__(self):              # __unicode__ on Python 2
+        return self.descripcion
